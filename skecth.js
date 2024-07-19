5ae1ed20011cb75e417573795accbbf6b1fd6aab
EdTecSalles <ednilsonsantos@prof.educacao.sp.gov.br>
2024-07-19 18:11:47 -0300

    Create skecth.js

---
+++ skecth.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+  
+  background("orange");
+}
+
+function draw() {
+  
+  stroke("green");
+  fill("blue");
+
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 45);
+  }
+}
